395d5cb5caf0f703cb4742114de6c219e69efcdd
darkclif <morpheoosmatrix@gmail.com>
2018-01-30 13:32:26 +0100

    Initial commit

---
+++ app/src/main/java/com/example/practicalistviewpersonalizado/Adaptadorcoche.java
@@ -0,0 +1,61 @@
+package com.example.practicalistviewpersonalizado;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by DIDACT on 30/01/2018.
+ */
+
+public class Adaptadorcoche extends ArrayAdapter<Coche> {
+
+    ArrayList<Coche> coches;
+    Context c;
+
+    public Adaptadorcoche(Context c,ArrayList<Coche> coches){
+
+        super(c, R.layout.item_coches, coches);
+        this.c =c;
+        this.coches = coches;
+
+
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        LayoutInflater inflater= LayoutInflater.from(getContext());
+        View item = inflater.inflate(R.layout.item_coches,null);
+
+        //TextView nombre
+        TextView tv_marca=(TextView)item.findViewById(R.id.item_tvmarca);
+        tv_marca.setText(coches.get(position).getMarca());
+
+        //TextView estadieo
+        TextView tv_modelo=(TextView)item.findViewById(R.id.item_tvmodelo);
+        tv_modelo.setText(coches.get(position).getModelo());
+
+        //ImageView
+        String imagen = coches.get(position).getImagen();
+
+        int idimagen = c.getResources().getIdentifier(imagen,"drawable",c.getPackageName());
+
+        ImageView iv_logo = (ImageView)item.findViewById(R.id.item_imagencoche);
+
+        iv_logo.setImageResource(idimagen);
+
+
+        return item;
+    }
+}
---
+++ app/src/main/java/com/example/practicalistviewpersonalizado/Coche.java
@@ -0,0 +1,57 @@
+package com.example.practicalistviewpersonalizado;
+
+/**
+ * Created by DIDACT on 30/01/2018.
+ */
+
+public class Coche {
+
+    String marca;
+    String modelo;
+    String imagen;
+    Double precio;
+
+    public Coche(String marca, String modelo, String imagen, Double precio) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.imagen = imagen;
+        this.precio = precio;
+
+
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+}
+
+
+
